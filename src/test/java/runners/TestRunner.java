9488138f4041cdbe9277a399063b200a1b44897d
Ravichandran Thiyagarajan <ravichandran.thiyagarajan@expleogroup.com>
2026-06-15 16:07:09 +0530

    test: Add TestNG Cucumber runner

---
+++ src/test/java/runners/TestRunner.java
@@ -0,0 +1,26 @@
+package runners;
+
+import io.cucumber.testng.AbstractTestNGCucumberTests;
+import io.cucumber.testng.CucumberOptions;
+import org.testng.annotations.DataProvider;
+
+@CucumberOptions(
+        features = "src/test/resources/features/",
+        glue = {"stepdefinitions"},
+        plugin = {
+                "pretty",
+                "html:target/cucumber-reports/auth-report.html",
+                "json:target/cucumber-reports/auth-report.json"
+        },
+        monochrome = true,
+        dryRun = false
+)
+public class TestRunner extends AbstractTestNGCucumberTests {
+
+    @Override
+    @DataProvider(parallel = false)
+    public Object[][] scenarios() {
+        return super.scenarios();
+    }
+}
+
